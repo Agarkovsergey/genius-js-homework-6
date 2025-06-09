4405f54a75d4e572047c32eb2436848ccc845218
Sergey Agarkov <grey1701@gmail.com>
2025-06-09 16:17:48 +0300

    hw6

--- script/script.js
+++ script/script.js
@@ -5,3 +5,30 @@
 // властивостях об’єкта.
 // sum() повертає суму цих властивостей.
 // mul() повертає результат множення даних властивостей.
+let output = document.getElementById('output');
+
+function Calculator() {
+  this.read = function () {
+    a = +prompt('введіть перше число:', 0);
+    b = +prompt('введіть друге число:', 0);
+    return a, b;
+  };
+  this.sum = function () {
+    return a + b;
+  };
+  this.mul = function () {
+    return a * b;
+  };
+}
+const calc = new Calculator();
+calc.read();
+
+const p0 = document.createElement('p');
+const p1 = document.createElement('p');
+const p2 = document.createElement('p');
+p0.textContent = 'Перше та друге число: ' + a + ' та ' + b;
+output.appendChild(p0);
+p1.textContent = 'Сума: ' + calc.sum();
+output.appendChild(p1);
+p2.textContent = 'Добуток: ' + calc.mul();
+output.appendChild(p2);
